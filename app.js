0a49d16b71c34bc6f7066a13b75fa8b1b5db9f68
Hugo <h.domingos@ua.pt>
2022-03-10 17:29:49 +0000

    chat working and gitignore

--- app.js
+++ app.js
@@ -9,14 +9,39 @@ const Socketio = require("socket.io")(Http, {
 
 var connections = [];
 
-Socketio.on("connection", socket => {
-    connections.push(socket.id);
-    Socketio.emit("connections", connections);
+var chat = {
+    "Room1" : [],
+    "Room2" : [],
+    "Room3" : [],
+};
 
+Socketio.on("connection", socket => {
+    // When a new socket connects its pushed to the connections array
+    connections.push(socket.id);
+     
+    Socketio.emit("socket_id", socket.id);
+
+    // sends all the socket connections
+    Socketio.emit("connections", connections);
+    
+    // when a socket disconnects its removed from the connections array
     socket.on("disconnect", () => {
         connections.splice(connections.indexOf(socket.id), 1);
         console.log(connections.toString());
         Socketio.emit("connections", connections);
+    });
+    
+    // store chat messages
+    socket.on("send_message", (data, room_id) => {
+        chat[room_id].push({'src': socket.id , 'data': data});
+        Socketio.emit("chat", chat[room_id]);
+        console.log(chat);
+    });
+    
+    //load chat messages when enters
+    socket.on("change_room", room_id => {
+        Socketio.emit("chat", chat[room_id]);
+        console.log(chat[room_id]);
     });
 
 });
