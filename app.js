f1dff3733a38273fd614ff68a09b634077db5fa3
Hugo <h.domingos@ua.pt>
2022-03-17 16:49:40 +0000

    game working

--- app.js
+++ app.js
@@ -11,13 +11,59 @@ var connections = [];
 
 var users = [];
 
+var rooms = ["Room1", "Room2", "Room3"];
+
 var chat = {
     "Room1" : [],
     "Room2" : [],
     "Room3" : [],
 };
 
+// Questions just for testing purposes
+var questions = [
+    {id:1, question: "Question1", options: ["a", "b", "c", "d"], answer: "a"},
+    {id:2, question: "Question2", options: ["a", "b", "c", "d"], answer: "b"},
+    {id:3, question: "Question3", options: ["a", "b", "c", "d"], answer: "c"},
+    {id:4, question: "Question4", options: ["a", "b", "c", "d"], answer: "d"}
+];
+
+var rooms_idx = {
+    "Room1" : Math.floor(Math.random() * questions.length),
+    "Room2" : Math.floor(Math.random() * questions.length),
+    "Room3" : Math.floor(Math.random() * questions.length),
+};
+
+var counter = 11;   // countdown timer value
+
+setInterval(function() {
+    console.log(counter);
+    counter--;
+    if(counter >= 0) {
+        Socketio.emit("counter", counter);
+    } else if(counter == -1) {
+        console.log("Showing result!");
+        // show the result of the answer
+        Socketio.emit("show_result");
+    } else if(counter == -3) {
+        console.log("Reseting!");
+
+        Socketio.emit("show_result");
+
+        // generate another random question for each room
+        rooms.forEach(element => { 
+            rooms_idx[element] = Math.floor(Math.random() * questions.length);
+        });
+        // send the new question for each room 
+        rooms.forEach(element => {
+            Socketio.to(element).emit("server_get_question", questions[rooms_idx[element]]);
+        });
+        console.log(rooms_idx);
+        counter = 11; 
+    } 
+}, 1000);
+
 Socketio.on("connection", socket => {
+
     // When a new socket connects its pushed to the connections array
     connections.push(socket.id);
 
@@ -33,7 +79,30 @@ Socketio.on("connection", socket => {
         users.splice(users[socket.id],1);
         Socketio.emit("connections", connections);
     });
+
+    // Send random question to clients in room
+    socket.on("client_get_question", (room_id) => {
+        // Socketio.to(room_id).emit("server_get_question", questions[room_id]);
+        for(let i = 0; i < rooms.length; i++) {
+            Socketio.to(rooms[i]).emit("server_get_question", questions[rooms[i]]);
+        }
+        console.log(rooms_idx)
+    });    
     
+    // refresh question every 10 seconds
+//    setInterval(function refreshQuestion() { 
+//        rooms.forEach(element => { 
+//            rooms_idx[element] = Math.floor(Math.random() * questions.length);
+//        });
+//
+//        console.log(rooms_idx);
+//        
+//        rooms.forEach(element => {
+//            Socketio.to(element).emit("server_get_question", questions[rooms_idx[element]]);
+//        });
+//        Socketio.emit("show_answer");
+//    }, 10000);
+
     // store chat messages
     socket.on("send_message", (data, room_id) => {
         socket.join(room_id);
@@ -46,17 +115,15 @@ Socketio.on("connection", socket => {
     socket.on("change_room", room_id => {
         socket.join(room_id);
         Socketio.to(socket.id).emit("chat", chat[room_id]);
-        console.log(chat[room_id]);
-        console.log(room_id);
+        Socketio.to(socket.id).emit("question_change_room", questions[rooms_idx[room_id]]); 
     });
 
     //tells the name of the user that connected
-    socket.on("nname",username =>{
+    socket.on("nname",username => {
         users[socket.id]=username;
-    });
-    
-});
+    });  
 
+});
 
 Http.listen(3000, () => {
     console.log("Listening at port 3000!");
